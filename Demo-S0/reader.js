ad2d7e61aa19c7e5b7b5871a2116e62a24270819
John Valentine <j5v@users.noreply.github.com>
2016-06-30 23:53:47 +0100

    Closure, description, single-purpose module
    1. `CodedRaw` now uses the closure pattern to publish the `CodedRaw` object, allowing a published interface and private data.
    2. Added a description of the demo, on the HTML page.
    3. Single-purpose refactoring: `begin()` did not belong in CodedRaw, so has been (simplified and) moved to an inline script in the HTML.
    
    Note that these new files were added to the wrong folder on the previous code commit.

--- Demo-S0/reader.js
+++ Demo-S0/reader.js
@@ -1,51 +1,50 @@
 // JavaScript for a Coded Raw client. Level S0.
 
-CodedRaw = {
-	getModule: function(filename, callback) {
-		var httpRequest = new XMLHttpRequest();;
-		console.log('Requesting "' + filename + '"');
-			httpRequest.onreadystatechange = function(){
-			if (httpRequest.readyState === XMLHttpRequest.DONE) {
-				console.log('httpRequest.status: ' + httpRequest.status);
-				if (httpRequest.status === 200) {
-			    	callback(JSON.parse(httpRequest.responseText));
-				} else {
-					console.log('error: CodedRaw file "' + filename + '" not received.');
+// Implemented as a closure: the interface is returned, all else is private.
+
+CodedRaw = (function () {
+
+	var
+		getModule = function (filename, callback) {
+
+			var httpRequest = new XMLHttpRequest();
+			console.log('Requesting "' + filename + '"');
+				httpRequest.onreadystatechange = function(){
+				if (httpRequest.readyState === XMLHttpRequest.DONE) {
+					console.log('httpRequest.status: ' + httpRequest.status);
+					if (httpRequest.status === 200) {
+				    	callback(JSON.parse(httpRequest.responseText));
+					} else {
+						console.log('error: CodedRaw file "' + filename + '" not received.');
+					}
 				}
-			}
+			};
+			httpRequest.open('GET', filename, true);
+			httpRequest.send();
+
+		},
+
+		render = function (mod) {
+
+			console.log('\nInterrogating file');
+
+			var getVersion = new Function(mod.code.getVersion);
+			console.log('File says it implements CodedRaw standard "' + getVersion() + '"');
+
+			console.log('File has JSON-safe encoded raw data: (next line)');
+			console.log(mod.data);
+
+			console.log('File renders data as: (next line)');
+			var getContent = new Function('data', mod.code.getContent);
+			console.log(getContent(mod.data));
+
 		};
-		httpRequest.open('GET', filename, true);
-		httpRequest.send();
-	},
 
-	render: function(mod) {
-
-		console.log('\nInterrogating file');
-
-		var getVersion = new Function(mod.code.getVersion);
-		console.log('File says it implements CodedRaw standard "' + getVersion() + '"');
-
-		console.log('File has JSON-safe encoded raw data: (next line)');
-		console.log(mod.data);
-
-		console.log('File renders data as: (next line)');
-		var getContent = new Function('data', mod.code.getContent);
-		console.log(getContent(mod.data));
-
+	return { // public interface
+		getModule,
+		render
 	}
 
-};
-
-
-// loader is just for the HTML client.
-begin = function() {
-	document.getElementById('loading').innerHTML='';
-	CodedRaw.files = [
-		{filename: 'S0-demo.json', description: 'Demo of a "Level 0" standard file.', selected: true},
-		{filename: 'S1-demo.json', description: 'X Demo of a "Level 1" standard file.'},
-		{filename: 'S2-demo.json', description: 'X Demo of a "Level 2" standard file.'},
-		{filename: 'S3-demo.json', description: 'X Demo of a "Level 3" standard file.'}
-	];
-};
+})();
 
 
