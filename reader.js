d6f6a80d892c3aae11f0a02ca25da550d1378216
John Valentine <j5v@users.noreply.github.com>
2016-06-30 23:50:41 +0100

    closure pattern, description, single-purpose JS
    1. `CodedRaw` now uses the closure pattern to publish the `CodedRaw` object, allowing a published interface and private data.
    2. Added a description of the demo, on the HTML page.
    3. Single-purpose refactoring: `begin()` did not belong in CodedRaw, so has been (simplified and) moved to an inline script in the HTML.

---
+++ reader.js
@@ -0,0 +1,52 @@
+// JavaScript for a Coded Raw client. Level S0.
+
+// Implemented as a closure: the interface is returned, all else is private.
+
+CodedRaw = (function () {
+
+	var
+		cache = undefined, // protected, persistent
+
+		getModule = function (filename, callback) {
+
+			var httpRequest = new XMLHttpRequest();
+			console.log('Requesting "' + filename + '"');
+				httpRequest.onreadystatechange = function(){
+				if (httpRequest.readyState === XMLHttpRequest.DONE) {
+					console.log('httpRequest.status: ' + httpRequest.status);
+					if (httpRequest.status === 200) {
+				    	callback(JSON.parse(httpRequest.responseText));
+					} else {
+						console.log('error: CodedRaw file "' + filename + '" not received.');
+					}
+				}
+			};
+			httpRequest.open('GET', filename, true);
+			httpRequest.send();
+
+		},
+
+		render = function (mod) {
+
+			console.log('\nInterrogating file');
+
+			var getVersion = new Function(mod.code.getVersion);
+			console.log('File says it implements CodedRaw standard "' + getVersion() + '"');
+
+			console.log('File has JSON-safe encoded raw data: (next line)');
+			console.log(mod.data);
+
+			console.log('File renders data as: (next line)');
+			var getContent = new Function('data', mod.code.getContent);
+			console.log(getContent(mod.data));
+
+		};
+
+	return { // public interface
+		getModule,
+		render
+	}
+
+})();
+
+
